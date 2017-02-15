2ed329c0d00d11abeae7a023f99d3c2f0e427751
Maaz Irfan <muhammadmaazirfan@outlook.com>
2017-02-15 20:19:45 +0500

    Sky Box change on run time

---
+++ skybox.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Rendering;
+
+public class skybox : MonoBehaviour {
+
+	public Material[] secondSkybox;
+	public static int i = 0;
+	public void skyboxOn()
+	{
+
+		if (i == 0) {
+
+		RenderSettings.skybox = secondSkybox[0];
+			i++;
+		}
+		else if(i==1)
+		{
+			RenderSettings.skybox = secondSkybox[1];
+			i++;
+		}else if(i==2)
+		{
+			RenderSettings.skybox = secondSkybox[2];
+			i=0;
+		}
+
+
+
+	}
+}
+
