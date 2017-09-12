e067261e58e94df901593f34bf4efac23831e604
Maaz Irfan <muhammadmaazirfan@outlook.com>
2017-09-12 20:03:24 +0500

    FPS First Person shooter Script

---
+++ FPS(shooter Script)/FireShoot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FireShoot : MonoBehaviour {
+
+    public Button  FireButton;
+
+    void Start () {
+        FireButton.onClick.AddListener(OnButtonDown);
+    }
+	
+
+
+    void OnButtonDown()
+    {
+
+        GameObject bullet = Instantiate(Resources.Load("bullet", typeof(GameObject))) as GameObject;
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        bullet.transform.rotation = Camera.main.transform.rotation;
+        bullet.transform.position = Camera.main.transform.position;
+        rb.AddForce(Camera.main.transform.forward * 500f);
+        Destroy(bullet, 3);
+
+
+    }
+
+
+}
